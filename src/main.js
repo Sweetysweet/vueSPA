9c74f8f15ad081421ad811a9270495389a283676
Sweetysweet <gsm_v@mail.ru>
2020-02-13 16:39:24 +0300

    ready SPA

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Meta from 'vue-meta'
+import App from './App.vue'
+import Post from './components/Post.vue'
+import Hello from './components/Hello.vue'
+
+Vue.use(Router)
+Vue.use(Meta)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Hello,
+    },
+    {
+      path: '/posts/:id',
+      name: 'post',
+      component: Post,
+      props: true,
+    },
+  ]
+})
+
+new Vue ({
+  el: '#app',
+  render: h => h(App),
+  router
+})
